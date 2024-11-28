16c190e2f67408a931fe87f22483372cc74cc1c8
seeyeon <gulf72pr@swu.ac.kr>
2024-11-28 15:09:55 +0900

    fun plus r

---
+++ src/test/java/com/ll/SimpleCalculatorTest.java
@@ -0,0 +1,19 @@
+package com.ll;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class SimpleCalculatorTest {
+    @Test
+    @DisplayName("1+2=3")
+    public void testPlus(){
+        SimpleCalculator simpleCalculator= new SimpleCalculator();
+        int rs = simpleCalculator.plus(1,2);
+
+        assertEquals(3, rs);
+    }
+
+}
