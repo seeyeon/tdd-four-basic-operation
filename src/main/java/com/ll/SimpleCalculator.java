c783db112196a483d10fdb25519f07caf60d0140
seeyeon <gulf72pr@swu.ac.kr>
2024-11-28 15:27:39 +0900

    fun plus3 b

--- src/main/java/com/ll/SimpleCalculator.java
+++ src/main/java/com/ll/SimpleCalculator.java
@@ -2,15 +2,8 @@ package com.ll;
 
 public class SimpleCalculator {
     public int plus(int num1, int num2) {
-        if(num1==10 && num2==2){
-            return 12;
-        }
 
-        if(num1==10 && num2==20){
-            return 30;
-        }
-
-        return 3;
+        return num1+num2;
     }
 }
 
