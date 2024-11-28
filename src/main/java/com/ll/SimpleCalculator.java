f829041cb05390bf330297a9bba2cd11c579cd6d
seeyeon <gulf72pr@swu.ac.kr>
2024-11-28 16:47:18 +0900

    fun multiply g/b

--- src/main/java/com/ll/SimpleCalculator.java
+++ src/main/java/com/ll/SimpleCalculator.java
@@ -9,5 +9,9 @@ public class SimpleCalculator {
     public int minus(int num1, int num2) {
         return num1-num2;
     }
+
+    public int multiply(int num1, int num2) {
+        return num1 * num2;
+    }
 }
 
